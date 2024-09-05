d2e44c6d38be66bbc673b60fc22b161ff940a588
Augustine Wigle <augustine.wigle@gmail.com>
2024-09-05 14:40:25 -0400

    Redo analysis with method.tau = "reml"

--- analyse.R
+++ analyse.R
@@ -1,29 +1,48 @@
 # Libraries ---------------------------------
-library("nmadb")
-library("netmeta")
+remotes::install_github("augustinewigle/nmadb")
+library(nmadb)
+library(netmeta)
 
 # Analyse the database
 studyList <- getNMADB()
-binaryStudiesIDs <-
-  subset(studyList, Effect.Measure == "odds ratio" & dataset != "")$Record.ID
+
 allStudyIDs <- subset(studyList, dataset != "")$Record.ID
 
-allNMAs <-
-  lapply(allStudyIDs,
-         #head(allStudyIDs, 3),
-         function(id) {
-           tryCatch({
-             net <- runnetmeta(recid=id)
-             return(list(recid = id, netobj = net))
-           },
-           error = function(cond) {
-             res <- paste(id,conditionMessage(cond))
-             res
-           }
-           )
-         }
-  )
 
-# Save the objects as RDS
-saveRDS(allNMAs, file = "allNMAs.rds")
+allNMAs <-
+  lapply( allStudyIDs
+        , function(id){
+            tryCatch({
+              net <- runnetmeta(recid=id, method.tau = "REML")
+              return(list(recid = id
+                         ,netobj = net))
+              }, error = function(cond) {
+                res <- paste(id,conditionMessage(cond))
+                res
+              }
+            )
+        })
+
+# Remove unnecessary returned information due to method.tau = REML
+ix <- c(156, 157, 158, 159, 160)
+
+listfun <- function(x) {
+
+  if(length(x) == 1) {
+
+    return(x)
+
+  }
+  recid <- x$recid
+  netobj <- x$netobj[-c(ix)]
+
+  class(netobj) <- "netmeta"
+  return(list(recid = recid, netobj = netobj))
+}
+
+
+allNMAsnew <- lapply(allNMAs, listfun)
+
+
+saveRDS(allNMAsnew, file = "allNMAs.rds")
 saveRDS(studyList, file = "studyList.rds")
