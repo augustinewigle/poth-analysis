7135f37f9ea9c97e876a0ccb99bfaf3ba8009e98
Augustine Wigle <augustine.wigle@gmail.com>
2024-09-05 14:41:15 -0400

    Update examples, sir -> poth

--- examples.R
+++ examples.R
@@ -1,118 +1,87 @@
 # Libraries
-library("tidyverse")
+library("tidyr")
+library("dplyr")
 library("netmeta")
-#remotes::install_github("audrey-b/BUGSnet")
-library("BUGSnet")
-library("sir")
+library("poth")
+library("ggplot2")
+library("ggpubr")
+# Safety of Immune checkpoint inhibitors in cancer -------------------------------
+
+xu <- read.csv("page_3_table.csv")
+trtnames <- c("conventional therapy",
+              "nivolumab",
+              "pembrolizumab",
+              "two ICIs",
+              "ICI and conventional",
+              "atezolizumab",
+              "ipilimumab")
+
+xu <- mutate(xu,
+             TreatmentID = trtnames[xu$TreatmentID])
+pw <- pairwise(treat = TreatmentID, event = Eventcount, n = Samplesize, studlab = StudyID, data = xu, sm = "OR")
+netw <- netmeta(pw, small.values = "desirable", method.tau = "REML", common = F)
+poth.ici <- poth(netw)
+poth.ici
+indiv.ici <- loo(poth.ici)
+indiv.ici
+cum.ici <- cumul(poth.ici)
+cum.ici
+
+l <-plot(indiv.ici)
+# ggsave("loo-ici.png", width = 4, height = 4, units = "in")
+
+subset.ici <- subset(poth.ici, subset = c("nivolumab",
+                                          "pembrolizumab",
+                                          "atezolizumab",
+                                          "ipilimumab"))
+
+subset.ici
+
+p <- plot(cum.ici, labels = F)
+
+ggarrange(p,l, nrow = 1, widths = c(1.2,2))
+# ggsave("diag-ici.png", width = 9, height = 3, units = "in")
+
+adddata <- indiv.ici$resid
+names(adddata) <- (indiv.ici$trt)
+ad <- data.frame(residuals = adddata[sort(trtnames)])
+# png("forest-ici.png",width = 8, height = 2.8, units = "in", res = 320)
+forest(netw, small.values = "desirable", reference.group = "conventional therapy",
+       sortvar = "-Pscore", rightcols = c("Pscore","residuals", "ci"), add.data = ad,
+       rightlabs = c(NA, "rPOTH", NA),
+       digits = 3,
+       smlab= "Comparison: other vs conventional therapy\nPOTH = 0.838")
+# dev.off()
 
 
-# Diabetes prevention example ------------------------------------------------
-data("diabetes")
-
-dat <- data.prep(arm.data = diabetes,
-                 varname.t = "Treatment",
-                 varname.s = "Study")
-
-random_effects_model <- nma.model(data = dat,
-                                  outcome = "diabetes",
-                                  N = "n",
-                                  reference = "Diuretic",
-                                  family = "binomial",
-                                  link = "cloglog",
-                                  time = "followup",
-                                  effects = "random")
-
-set.seed(2024)
-random_effects_results <- nma.run(random_effects_model,
-                                  n.adapt = 1000,
-                                  n.burnin = 5000,
-                                  n.iter = 10000)
-
-cat(random_effects_results$model$bugs)
-
-# Matrix of relative effect samples
-all_samps <- as.matrix(random_effects_results$samples)
-effect_samps <-
-  all_samps[, startsWith(colnames(all_samps),prefix = "d[")]
-
-# SIR and LOO SIR
-
-sir1 <- sir(effect_samps,
-            trts = c("Diuretic",
-                     "ACE inhibitor",
-                     "ARB",
-                     "blocker",
-                     "CCB",
-                     "Placebo"),
-            small.values = "desirable")
-
-loo1 <- loo(sir1)
-
-plot(loo1)
-
-loo1
-
-nma.forest(random_effects_results,
-           comparator = "Placebo",
-           order = rev(loo1$info$trt_name))
-nma.league(random_effects_results)
-
-# local SIR - the top 3 treatments
-loc1 <- loo(subset(sir1, top = 3))
-
-plot(loc1)
-
-
-
-
-
+# Depression example - many treatments ---------------------------------------
 # Load the object of all NMA analyses
 allNMAs <- readRDS("allNMAs.rds")
 study_info <- readRDS("studyList.rds") %>%
   select(-contains("..choice"))
-#
+
 ran <- unlist(lapply(allNMAs, function(x) length(x) > 1))
 ranNMAs <- allNMAs[which(ran)]
 
+# Depression example
+id_depression <- 48
 
-# Depression example - many treatments ---------------------------------------
-id_depression <- 49
-recid_depression <- ranNMAs
+recid_depression <- ranNMAs[[id_depression]]$recid
 ex_depression <- ranNMAs[[id_depression]]$netobj
 
 # Find out info about study, like the study name - this is how treatment names are identified
 depression_info <- subset(study_info, Record.ID == recid_depression) %>%
   select(-contains("..choice"))
 
-trt_names <- c("placebo",
-               "fluoxetine",
-               "sertraline",
-               "paroxetine",
-               "reboxetine",
-               "viloxazine",
-               "moclobemide",
-               "phenelzine",
-               "clomipramine",
-               "imipramine",
-               "amineptine",
-               "amitriptyline",
-               "desipramine",
-               "trazodone",
-               "minaprine",
-               "st. john's wort",
-               "ritanserin",
-               "amisulpride",
-               "flupenthixol",
-               "lorazepam",
-               "diazepam",
-               "acetyl-l-carnitine",
-               "fluoxetine + bentazepam",
-               "paroxetine + amisulpride",
-               "escitalopram",
-               "dothiepine",
-               "duloxetine",
-               "buprion + escitalopram",
-               "venlafaxine + mirtazapine")
+trt_names <-
+  c("placebo", "fluoxetine", "sertraline", "paroxetine", "reboxetine",
+    "viloxazine", "moclobemide", "phenelzine", "clomipramine",
+    "imipramine", "amineptine", "amitriptyline", "desipramine",
+    "trazodone", "minaprine", "st. john's wort", "ritanserin",
+    "amisulpride", "flupenthixol", "lorazepam", "diazepam",
+    "acetyl-l-carnitine", "fluoxetine + bentazepam",
+    "paroxetine + amisulpride", "escitalopram", "dothiepine",
+    "duloxetine", "buprion + escitalopram", "venlafaxine + mirtazapine")
 
 newdata_depression <-
   ex_depression$data %>%
@@ -120,43 +89,46 @@ newdata_depression <-
          trt2_new = trt_names[as.numeric(treat2)])
 
 net.depr <- netmeta(TE, seTE, trt1_new, trt2_new, studlab,
+                    method.tau = "REML",
                     data = newdata_depression,
                     sm = ex_depression$sm,
                     details.chkmultiarm = TRUE,
                     common = FALSE)
 
-# Calculate SIR and do LOO SIR
+# Calculate poth and do LOO poth
+poth.depr <- poth(net.depr, small.values = "undesirable")
+poth.depr
 
-sir.depr <- sir(net.depr, small.values = "undesirable")
-sir.depr
 
-loo.depr <- loo(sir.depr)
+indiv.depr <- loo(poth.depr)
+cum.depr <- cumul(poth.depr)
 
-plot(loo.depr)
 
+p1 <- plot(indiv.depr)
+p2 <- plot(cum.depr)
+ggarrange(p1,p2, heights = c(2, 1.1), nrow = 2)
+# ggsave("diag-depr.png", width = 9, height = 5, units = "in")
+
+# plot(cumul(poth.depr), labels = F)
+# ggsave("cum-depr.png",width = 8, height = 5, units = "in")
+# forest plot
+res <- indiv.depr$resid
+names(res) <- (indiv.depr$trt)
+dat <- data.frame(residuals = res[sort(trt_names)])
+
+# png("forest-depr.png", width = 8, height =7, res = 320, units = "in")
 forest(net.depr, small.values = "undesirable", reference.group = "placebo",
-       sortvar = "-Pscore", rightcols = c("Pscore","effect", "ci"),
-       smlab= "Comparison: other vs placebo\nSIR = 0.538")
+       sortvar = "-Pscore", rightcols = c("Pscore","residuals", "ci"),
+       rightlabs = c(NA,"rPOTH",NA),
+       add.data = dat,
+       digits = 3,
+       smlab= "Comparison: other vs placebo\nPOTH = 0.559")
 
+# dev.off()
 
-# Local SIR - Look at top 5 treatments
-top5.depr <- subset(sir.depr, top = 5)
-loo.top5.depr <- loo(top5.depr)
+# Transplants - low poth ---------------------------------------------------
+num <- 227
 
-top5
-plot(loo.top5.depr)
-
-# Local Sir - look at top and bottom 5
-
-top5.bottom5.depr <- subset(sir.depr, top = 5, bottom = 5)
-loo.top5.bottom5.depr <- loo(top5.bottom5.depr)
-
-top5.bottom5.
-plot(loo.top5.bottom5.)
-
-
-# Transplants - low SIR ---------------------------------------------------
-num <- 229
 low_recid <- ranNMAs[[num]]$recid
 low_nma <- ranNMAs[[num]]$netobj
 
@@ -166,11 +138,9 @@ low_info <- subset(study_info, Record.ID == low_recid) %>%
          Type.of.Outcome., Effect.Measure, Verified, Error)
 low_nma$data %>% select(study, treat1, treat2, TE, event1, n1, event2, n2)
 
-trt_names <- c("Control",
-               "Fluconazole",
-               "Itraconazole",
-               "Liposomal amphotericin B",
-               "Ketoconazole")
+trt_names <-
+  c("Control", "Fluconazole", "Itraconazole", "Liposomal amphotericin B",
+    "Ketoconazole")
 
 lowdata2 <- low_nma$data %>%
   mutate(trt1_new = trt_names[as.numeric(treat1)],
@@ -179,24 +149,46 @@ lowdata2 <- low_nma$data %>%
 net.transp <- netmeta(TE, seTE, trt1_new, trt2_new, studlab,
                       data = lowdata2,
                       sm = low_nma$sm,
+                      method.tau = "REML",
                       common = FALSE)
 
-forest(net.transp, small.values = "good", reference.group = "Control",
-       sortvar = "-Pscore", rightcols = c("Pscore","effect", "ci"),
-       smlab= "Comparison: other vs control\nSIR = 0.241")
+poth.transp <- poth(net.transp)
+poth.transp
 
-sir.transp <- sir(net.transp)
-sir.transp
+indiv.transp <- loo(poth.transp)
 
-loo3 <- loo(sir.transp)
-plot(loo3)
+cum.transp <- cumul(poth.transp)
 
-# local SIR - compare all non-control treatments
-loc3 <- subset(sir.transp,
-               subset = c("Fluconazole",
-                          "Itraconazole",
-                          "Liposomal amphotericin B",
-                          "Ketoconazole"))
-loc3
+a <- plot(indiv.transp)
 
-plot(loc3)
+b <- plot(cum.transp)
+
+ggarrange(b, a, widths = c(1.2, 2), nrow = 1)
+# ggsave("diag_transp.png", width = 9, height = 3, units = "in")
+# forest plot
+res <- indiv.transp$resid
+names(res) <- (indiv.transp$trt)
+dat <- data.frame(residuals = res[sort(trt_names)])
+
+# png("forest-transp.png", width = 8, height =2.5, res = 320, units = "in")
+forest(net.transp, small.values = "desirable", reference.group = "Control",
+       sortvar = "-Pscore", rightcols = c("Pscore","residuals", "ci"),
+       rightlabs = c(NA,"rPOTH",NA),
+       add.data = dat,
+       digits = 3,
+       smlab= "Comparison: other vs control\nPOTH = 0.326")
+
+# dev.off()
+
+# cumulative POTH
+cum.transp <- cumul(poth.transp)
+plot(cum.transp)
+# ggsave("cum_transp.png", width = 8, height = 4, units = "in")
+
+#subset POTH
+sub.transp <- subset(poth.transp, c("Fluconazole",
+                                   "Itraconazole",
+                                   "Liposomal amphotericin B",
+                                   "Ketoconazole"))
+
+sub.transp # poth = 0.354
---
+++ poth_analysis.R
@@ -0,0 +1,228 @@
+# Libraries --------------------------
+library("netmeta")
+library("poth")
+library("dplyr")
+
+
+# helper functions for database analysis -----------------------------------------
+get_ranks <- function(obj, sel) {
+  nma <- obj$netobj
+  recid <- obj$recid
+  #
+  ok <- netrank(nma, method = "P-score",
+                small.values =
+                  sel$small.values[sel$recid == recid])
+  #
+  ok$Pscore.random
+}
+
+
+poth_func <- function(x)
+  poth(x, trts = seq_len(length(x)))$poth
+
+ns_func <- function(obj) {
+  nma <- obj$netob
+  ns <- obj$netobj$k
+  #
+  ns
+}
+
+prop_func <- function(obj, alpha = 0.05) {
+
+  mean(obj$netobj$pval.random < alpha, na.rm = T)
+
+}
+
+range_func <- function(pscores) {
+
+  max(pscores) - min(pscores)
+
+}
+
+q_func <- function(obj) {
+
+  nma <- obj$netob
+
+  q_overall <- obj$netobj$pval.Q
+  q_het <- obj$netobj$pval.Q.heterogeneity
+  q_inc <- obj$netobj$pval.Q.inconsistency
+
+  return(c(q_overall, q_het, q_inc))
+
+}
+
+get_sm <- function(obj) {
+
+  obj$netobj$sm
+
+}
+
+tau_func <- function(obj) {
+
+  obj$netobj$tau
+
+}
+
+
+# Calculate POTH from the analysed datasets -------------------------------------
+
+# First, use analyse.R to analyse all datasets yourself using nmadb
+# Or, use download the .rds object provided
+
+# Read in all NMAs analysis object and study information
+allNMAs <- readRDS("allNMAs.rds") # first, set working directory to location where you saved the object
+
+study_info <- readRDS("studyList.rds") %>% select(-contains("..choice"))
+
+# find only the ones without errors
+ran <- unlist(lapply(allNMAs, function(x) length(x) > 1))
+ran_NMAs <- allNMAs[which(ran)] # 266 ran
+
+error_NMAs <- allNMAs[-c(which(ran))] # 28 of them had errors
+
+# Get the record IDs of the NMAs which ran
+ran_NMAs_record <- unlist(lapply(ran_NMAs, function(x) x$recid))
+
+# figure out if effect is beneficial or harmful
+studies_ran <- subset(study_info,
+                      Record.ID %in% ran_NMAs_record) %>%
+  select(Record.ID, Harmful.Beneficial.Outcome) %>%
+  rename(recid = Record.ID) %>%
+  mutate(small.values =
+           if_else(Harmful.Beneficial.Outcome == "Beneficial",
+                   "undesirable", "desirable"))
+
+# Calculate poths for everything
+pscores <- lapply(ran_NMAs, get_ranks, sel = studies_ran)
+poths <- sapply(pscores, function(x) poth(x)$poth)
+summary(poths)
+
+
+# pdf("~/PhD/Freiburg/Project/SIR/results/hist_sir.pdf", width = 6, height = 4.5)
+hist(poths, main = "",
+     xlab= "POTH", col = "grey95", breaks = 32, xlim = c(0,1))
+abline(v = quantile(poths, c(0.5)), col = "black", lwd = 2, lty = 2)
+legend("topleft", text.col = c("black", "black", "black"), lty = c(2, NA, NA), lwd =c(2, NA, NA),
+       legend = c("Median = 0.672", "Min = 0.096", "Max > 0.999"), bty = "n")
+# dev.off()
+
+# number of studoes
+nstudies <- sapply(ran_NMAs,ns_func)
+
+# look at relationship with number of treatments
+ntrts <- sapply(pscores, length)
+cor(poths, ntrts)
+plot(ntrts, poths, ylim = c(0,1), xlim = c(4, 46),
+     xlab = "Number of Treatments", ylab = "POTH")
+
+# Relationship with proportion of significantly different treatment comparisons
+alphas <- rev(c(0.1, 0.05, 0.01, 0.005))
+proportions <- matrix(nrow = length(alphas), ncol = length(poths))
+
+for (i in 1:length(alphas))
+  proportions[i, ] <- sapply(ran_NMAs, prop_func, alpha = alphas[i])
+
+range(poths[which(proportions[3,] == 0)])
+
+plot(proportions[3,], poths,
+     xlab = "Proportion of treatment comparisons that are stat. signif. at 5% level",
+     ylab = 'POTH', ylim = c(0,1))
+
+# plots
+# pdf("~/PhD/Freiburg/Project/SIR/results/ntrt_alpha_sir.pdf", width = 8*1.5, height = 4*1.5)
+par(mfrow = c(1,2))
+plot(ntrts, poths, ylim = c(0,1), xlim = c(4, 46),
+     xlab = "Number of Treatments", ylab = "POTH")
+plot(proportions[3,], poths,
+     xlab = "Proportion of treatment comparisons that are stat. signif. at 5% level",
+     ylab = 'POTH', ylim = c(0,1))
+# dev.off()
+
+# Relationship with range of p-scores
+prange <- sapply(pscores, range_func)
+plot(prange, poths, xlim = c(0,1), ylim = c(0,1),
+     xlab = "max(P-scores) - min(p-scores)", ylab = "POTH")
+
+# Relationship with heterogeneity.inconsistency
+
+q_pvals <- sapply(ran_NMAs, q_func)
+row.names(q_pvals) <- c("overall", "heterogeneity", "inconsistency")
+
+
+par(mfrow = c(1,3))
+
+for(i in 1:3) {
+
+  plot(q_pvals[i,], poths, ylim = c(0,1),
+       xlab = paste0("P-value for Q (", row.names(q_pvals)[i], ")"), ylab = "POTH")
+  abline(v = 0.05, lty = 3, col = "grey")
+  print(cor(poths, q_pvals[i,]))
+
+}
+
+par(mfrow = c(1,1))
+
+# Look at tau for different effect measures
+
+taus <- sapply(ran_NMAs, tau_func)
+
+effs <- sapply(ran_NMAs, get_sm)
+table(effs)
+
+thresh <- 0 # can use to remove numerically zero taus if desired
+
+ors <- which(effs == "OR" & taus > thresh)
+mds <- which(effs == "MD" & taus > thresh)
+rrs <- which(effs == "RR" & taus > thresh)
+
+mdsno <- which(effs == "MD" & taus > thresh & taus < 30)
+
+summary(lm(poths[rrs] ~ taus[rrs]))
+summary(lm(poths[ors] ~ taus[ors]))
+summary(lm(poths[mds] ~ taus[mds]))
+
+cor(taus[ors], poths[ors])
+cor(taus[rrs], poths[rrs])
+cor(taus[mds], poths[mds])
+
+
+# pdf("~/PhD/Freiburg/Project/SIR/results/tau_sir.pdf", width = 8*1.2, height = 2.5*1.2)
+par(mfrow = c(1,3))
+plot(taus[ors], poths[ors], main = "Effect Measure: Log Odds Ratio\n119 NMAs",
+     xlab = expression(tau), ylab = "POTH",
+     ylim = c(0,1))
+plot(taus[rrs], poths[rrs], main = "Effect Measure: Log Relative Risk\n69 NMAs",
+     xlab = expression(tau), ylab = "POTH",
+     ylim = c(0,1))
+plot(taus[mdsno], poths[mdsno], main = "Effect Measure: Mean Difference\n44 NMAs",
+     xlab = expression(tau), ylab = "POTH",
+     ylim = c(0,1))
+
+# dev.off()
+
+# Comparisons to examples --------------------------------------------
+mean(poths>0.326) # antifungals for transplants example
+
+mean(poths > 0.838) # ICIs for cancer
+
+mean(poths > 0.559) # depression
+
+# max and min poth analyses ------------------------------------------
+maxid <- which(poths > 0.999)
+minid <- which(poths < 0.1)
+
+
+max_nma <- ran_NMAs[[maxid]]$netobj
+min_nma <- ran_NMAs[[minid]]$netobj
+# png("largest_poth.png", width = 6, height = 2.5, units = "in", res = 320)
+forest(max_nma)
+# dev.off()
+# png("smallest_poth.png", width = 6, height = 2.5, units = "in", res = 320)
+forest(min_nma)
+# dev.off()
+
+minid2 <- ran_NMAs[[minid]]$recid
+subset(study_info, Record.ID == minid2)
+
+maxid2 <- ran_NMAs[[maxid]]$recid
+subset(study_info, Record.ID == maxid2)
