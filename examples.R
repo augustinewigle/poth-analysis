84d6756626356f434a2674303fa14ee8fbd6dcbc
Guido Schwarzer <guido.schwarzer@uniklinik-freiburg.de>
2024-09-06 18:27:07 +0200

    R code for examples updated

--- examples.R
+++ examples.R
@@ -14,7 +14,31 @@ library("ggpubr")
 #
 
 load("nmadb_NMAs.rda")
-study_info <- studyList %>% select(-contains("..choice"))
+
+study_info <- studyList %>%
+  rename(recordid = Record.ID, author = First.Author,
+         title = Title, outcome = Description.of.the.outcome.s.,
+         small.values = Harmful.Beneficial.Outcome,
+         type = Type.of.Outcome., sm = Effect.Measure,
+         verified = Verified, error = Error) %>%
+  mutate(small.values =
+           recode(small.values, 
+                  "Beneficial" = "undesirable",
+                  "Harmful" = "desirable",
+                  "Unclear" = "unclear"),
+         sm =
+           recode(sm,
+                  "hazard ratio" = "HR",
+                  "mean difference" = "MD",
+                  "odds ratio" = "OR",
+                  "other" = "other",
+                  "rate ratio" = "IRR",
+                  "risk difference" = "RD",
+                  "risk ratio" = "RR",
+                  "standardized mean difference" = "SMD")) %>%
+  select(recordid, author, title, outcome,
+         small.values, type, sm, verified, error)
+
 
 ran <- sapply(allNMAs, function(x) length(x) > 1)
 ranNMAs <- allNMAs[ran]
@@ -23,204 +47,202 @@ recIDs <- sapply(ranNMAs, function(x) x$recid)
 
 #
 #
+# Example 1:
+# Antifungal Treatments to Prevent Mortality for Solid Organ Transplant
+# Recipients
+#
+#
+
+recid1 <- 501370
+#
+trts1 <-
+  c("Control", "Fluconazole", "Itraconazole", "Liposomal amphotericin B",
+    "Ketoconazole")
+
+nma1 <- ranNMAs[[which(recIDs == recid1)]]$netobj
+#
+info1 <- study_info %>% filter(recordid == recid1)
+info1
+#
+dat1 <- nma1$data %>%
+  mutate(trt1 = factor(treat1, levels = seq_along(trts1), labels = trts1),
+         trt2 = factor(treat2, levels = seq_along(trts1), labels = trts1))
+#
+dat1 %>% select(studlab, trt1, trt2, TE, event1, n1, event2, n2) %>% head()
+
+# It is important to define argument 'small.values' which is used to calculate
+# P-scores and POTHs
+net1 <- netmeta(TE, seTE, trt1, trt2, studlab, data = dat1,
+  sm = info1$sm, small.values = info1$small.values,
+  common = FALSE, method.tau = "REML")
+#
+poth1 <- poth(net1)
+loo1 <- loo(poth1)
+cum1 <- cumul(poth1)
+
+# Forest plot
+#
+add1 <- as.data.frame(loo1) %>% arrange(trt)
+all(net1$trts == add1$trt)
+#
+# png("forest-transp.png", width = 8, height =2.5, res = 320, units = "in")
+forest(net1, reference.group = "Control",
+       sortvar = "-Pscore",
+       add.data = add1,
+       leftcols = c("studlab", "Pscore", "resid"),
+       leftlabs = c(NA, NA, "rPOTH"),
+       #rightcols = c("Pscore", "resid", "effect", "ci"),
+       #rightlabs = c(NA, "rPOTH", NA, NA),
+       digits = 2, digits.prop = 3, digits.addcols = 3,
+       just.addcols = "right",
+       addrows = 1,
+       text.addline1 = paste("Global POTH =", round(poth1$poth, 3)),
+       ff.addline = "bold")
+#       smlab = paste("Comparison: other vs control\nPOTH =",
+#                     round(poth1$poth, 3)))
+# dev.off()
+
+# Bar plots
+#
+plt1.1 <- plot(loo1)
+plt1.2 <- plot(cum1)
+#plt1.2 <- plot(cum1, labels = TRUE)
+#
+ggarrange(plt1.2, plt1.1, widths = c(1.2, 2), nrow = 1)
+# ggsave("diag_transp.png", width = 9, height = 3, units = "in")
+
+# Subset POTH
+#
+sub1 <- subset(poth1,
+  c("Fluconazole", "Itraconazole",
+    "Liposomal amphotericin B", "Ketoconazole"))
+sub1 # poth = 0.354
+
+
+#
+#
+# Example 2:
+# Efficacy of Pharmacological Treatments for Persistent Depressive Disorder
+#
+#
+
+recid2 <- 480666
+#
+trts2 <-
+  c("Placebo", "Fluoxetine", "Sertraline", "Paroxetine", "Reboxetine",
+    "Viloxazine", "Moclobemide", "Phenelzine", "Clomipramine",
+    "Imipramine", "Amineptine", "Amitriptyline", "Desipramine",
+    "Trazodone", "Minaprine", "St. John's wort", "Ritanserin",
+    "Amisulpride", "Flupenthixol", "Lorazepam", "Diazepam",
+    "Acetyl-l-carnitine", "Fluoxetine + Bentazepam",
+    "Paroxetine + Amisulpride", "Escitalopram", "Dothiepine",
+    "Duloxetine", "Buprion + Escitalopram", "Venlafaxine + Mirtazapine")
+
+nma2 <- ranNMAs[[which(recIDs == recid2)]]$netobj
+#
+info2 <- study_info %>% filter(recordid == recid2)
+info2
+#
+dat2 <- nma2$data %>%
+  mutate(trt1 = factor(treat1, levels = seq_along(trts2), labels = trts2),
+         trt2 = factor(treat2, levels = seq_along(trts2), labels = trts2))
+#
+dat2 %>% select(studlab, trt1, trt2, TE, n1, n2) %>% head()
+
+# It is important to define argument 'small.values' which is used to calculate
+# P-scores and POTHs
+net2 <- netmeta(TE, seTE, trt1, trt2, studlab, data = dat2,
+  sm = info2$sm, small.values = info2$small.values,
+  common = FALSE, method.tau = "REML")
+#
+poth2 <- poth(net2)
+loo2 <- loo(poth2)
+cum2 <- cumul(poth2)
+
+# Forest plot
+#
+add2 <- as.data.frame(loo2) %>% arrange(trt)
+all(net2$trts == add2$trt)
+#
+# png("forest-depr.png", width = 8, height =7, res = 320, units = "in")
+forest(net2, reference.group = "placebo",
+       sortvar = "-Pscore",
+       add.data = add2,
+       leftcols = c("studlab", "Pscore", "resid"),
+       leftlabs = c(NA, NA, "rPOTH"),
+       #rightcols = c("Pscore", "resid", "effect", "ci"),
+       #rightlabs = c(NA, "rPOTH", NA, NA),
+       digits = 2, digits.prop = 3, digits.addcols = 3,
+       just.addcols = "right",
+       addrows = 1,
+       text.addline1 = paste("Global POTH =", round(poth2$poth, 3)),
+       ff.addline = "bold")
+#       smlab = paste("Comparison: other vs placebo\nPOTH =",
+#                    round(poth2$poth, 3)))
+# dev.off()
+
+# Bar plots
+#
+plt2.1 <- plot(loo2)
+plt2.2 <- plot(cum2)
+ggarrange(plt2.1, plt2.2, heights = c(2, 1.1), nrow = 2)
+# ggsave("diag-depr.png", width = 9, height = 5, units = "in")
+
+
+#
+#
+# Example 3:
 # Safety of Immune checkpoint inhibitors in cancer
 #
 #
 
-xu <- read.csv("page_3_table.csv")
-trtnames <- c("conventional therapy",
-              "nivolumab",
-              "pembrolizumab",
-              "two ICIs",
-              "ICI and conventional",
-              "atezolizumab",
-              "ipilimumab")
-
-xu <- mutate(xu, TreatmentID = trtnames[xu$TreatmentID])
-pw <- pairwise(treat = TreatmentID, event = Eventcount, n = Samplesize,
-  studlab = StudyID, data = xu, sm = "OR")
-net.ici <- netmeta(pw, small.values = "desirable", method.tau = "REML",
+data(Xu2018, package = "poth")
+#
+pw <- pairwise(treat = treatment, event = adverse, n = n,
+  studlab = studyID, data = Xu2018, sm = "OR")
+#
+net3 <- netmeta(pw, small.values = "desirable", method.tau = "REML",
   common = FALSE)
-poth.ici <- poth(net.ici)
-poth.ici
-loo.ici <- loo(poth.ici)
-loo.ici
-cum.ici <- cumul(poth.ici)
-cum.ici
+#
+poth3 <- poth(net3)
+loo3 <- loo(poth3)
+cum3 <- cumul(poth3)
 
-l <- plot(loo.ici)
-# l
-# ggsave("loo-ici.png", width = 4, height = 4, units = "in")
+# Forest plot
+#
+add3 <- as.data.frame(loo3) %>% arrange(trt)
+all(sort(net3$trts) == sort(add3$trt))
+add3 <- add3[net3$trts, ]
+#
+# png("forest-ici.png",width = 8, height = 2.8, units = "in", res = 320)
+forest(net3,
+       reference.group = "Conventional therapy",
+       sortvar = "-Pscore",
+       add.data = add3,
+       leftcols = c("studlab", "Pscore", "resid"),
+       leftlabs = c(NA, NA, "rPOTH"),
+       #rightcols = c("Pscore", "resid", "effect", "ci"),
+       #rightlabs = c(NA, "rPOTH", NA, NA),
+       digits = 2, digits.prop = 3, digits.addcols = 3,
+       just.addcols = "right",
+       addrows = 1,
+       text.addline1 = paste("Global POTH =", round(poth3$poth, 3)),
+       ff.addline = "bold")
+#       smlab =
+#        paste("Comparison: other vs Conventional therapy\nPOTH = ",
+#             round(poth3$poth, 3)))
+# dev.off()
 
-subset.ici <- subset(poth.ici,
-                     subset = c("nivolumab", "pembrolizumab",
-                                "atezolizumab", "ipilimumab"))
-
-subset.ici
-
-p <- plot(cum.ici, labels = FALSE)
-
-ggarrange(p, l, nrow = 1, widths = c(1.2, 2))
+# Bar plots
+#
+plt3.1 <- plot(loo3)
+plt3.2 <- plot(cum3)
+ggarrange(plt3.2, plt3.1, nrow = 1, widths = c(1.2, 2))
 # ggsave("diag-ici.png", width = 9, height = 3, units = "in")
 
-ici.add <- as.data.frame(loo.ici) %>% arrange(trt)
-all(sort(net.ici$trts) == sort(ici.add$trt))
-ici.add <- ici.add[net.ici$trts, ]
-
-# png("forest-ici.png",width = 8, height = 2.8, units = "in", res = 320)
-forest(net.ici, small.values = "desirable",
-       reference.group = "conventional therapy",
-       sortvar = "-Pscore",
-       add.data = ici.add,
-       leftcols = c("studlab", "Pscore", "resid"),
-       leftlabs = c(NA, NA, "rPOTH"),
-       #rightcols = c("Pscore", "resid", "effect", "ci"),
-       #rightlabs = c(NA, "rPOTH", NA, NA),
-       digits = 2, digits.prop = 3, digits.addcols = 3,
-       just.addcols = "right",
-       smlab =
-         paste("Comparison: other vs conventional therapy\nPOTH = ",
-               round(poth.ici$poth, 3)))
-
-# dev.off()
-
-
+# Subset POTH
 #
+sub3 <- subset(poth3,
+  subset = c("nivolumab", "pembrolizumab", "atezolizumab", "ipilimumab"))
 #
-# Depression example - many treatments
-#
-#
-
-recid_depression <- 480655
-ex_depression <- ranNMAs[[which(recIDs == recid_depression)]]$netobj
-
-# Find out info about study, like the study name - this is how treatment names are identified
-depression_info <- study_info %>% filter(Record.ID == recid_depression)
-
-trt_names <-
-  c("placebo", "fluoxetine", "sertraline", "paroxetine", "reboxetine",
-    "viloxazine", "moclobemide", "phenelzine", "clomipramine",
-    "imipramine", "amineptine", "amitriptyline", "desipramine",
-    "trazodone", "minaprine", "st. john's wort", "ritanserin",
-    "amisulpride", "flupenthixol", "lorazepam", "diazepam",
-    "acetyl-l-carnitine", "fluoxetine + bentazepam",
-    "paroxetine + amisulpride", "escitalopram", "dothiepine",
-    "duloxetine", "buprion + escitalopram", "venlafaxine + mirtazapine")
-
-newdata_depression <-
-  ex_depression$data %>%
-  mutate(trt1_new = trt_names[as.numeric(treat1)],
-         trt2_new = trt_names[as.numeric(treat2)])
-
-net.depr <-
-  netmeta(TE, seTE, trt1_new, trt2_new, studlab, data = newdata_depression,
-          sm = ex_depression$sm, common = FALSE, method.tau = "REML")
-
-# Calculate POTH and do LOO POTH
-
-poth.depr <- poth(net.depr, small.values = "undesirable")
-poth.depr
-
-loo.depr <- loo(poth.depr)
-cum.depr <- cumul(poth.depr)
-
-
-p1 <- plot(loo.depr)
-p2 <- plot(cum.depr)
-ggarrange(p1, p2, heights = c(2, 1.1), nrow = 2)
-# ggsave("diag-depr.png", width = 9, height = 5, units = "in")
-
-# plot(cumul(poth.depr), labels = FALSE)
-# ggsave("cum-depr.png",width = 8, height = 5, units = "in")
-# forest plot
-
-loo.add <- as.data.frame(loo.depr) %>% arrange(trt)
-all(net.depr$trts == loo.add$trt)
-
-# png("forest-depr.png", width = 8, height =7, res = 320, units = "in")
-forest(net.depr, small.values = "undesirable", reference.group = "placebo",
-       sortvar = "-Pscore",
-       add.data = loo.add,
-       leftcols = c("studlab", "Pscore", "resid"),
-       leftlabs = c(NA, NA, "rPOTH"),
-       #rightcols = c("Pscore", "resid", "effect", "ci"),
-       #rightlabs = c(NA, "rPOTH", NA, NA),
-       digits = 2, digits.prop = 3, digits.addcols = 3,
-       just.addcols = "right",
-       smlab = paste("Comparison: other vs placebo\nPOTH =",
-                    round(poth.depr$poth, 3)))
-
-# dev.off()
-
-
-#
-#
-# Transplants - low POTH
-#
-#
-
-recid.transp <- 501370
-nma_transp <- ranNMAs[[which(recIDs == recid.transp)]]$netobj
-
-info_transp <- subset(study_info, Record.ID == recid.transp) %>%
-  select(Record.ID, First.Author, Title, Description.of.the.outcome.s.,
-         Harmful.Beneficial.Outcome,
-         Type.of.Outcome., Effect.Measure, Verified, Error)
-info_transp
-nma_transp$data %>% select(study, treat1, treat2, TE, event1, n1, event2, n2)
-
-trt_names <-
-  c("Control", "Fluconazole", "Itraconazole", "Liposomal amphotericin B",
-    "Ketoconazole")
-
-data_transp <- nma_transp$data %>%
-  mutate(trt1_new = trt_names[as.numeric(treat1)],
-         trt2_new = trt_names[as.numeric(treat2)])
-
-net.transp <-
-  netmeta(TE, seTE, trt1_new, trt2_new, studlab, data = data_transp,
-          sm = nma_transp$sm, common = FALSE, method.tau = "REML")
-
-poth.transp <- poth(net.transp)
-poth.transp
-
-loo.transp <- loo(poth.transp)
-
-cum.transp <- cumul(poth.transp)
-
-p1 <- plot(loo.transp)
-p2 <- plot(cum.transp)
-
-ggarrange(p2, p1, widths = c(1.2, 2), nrow = 1)
-# ggsave("diag_transp.png", width = 9, height = 3, units = "in")
-# forest plot
-
-transp.add <- as.data.frame(loo.transp) %>% arrange(trt)
-all(net.transp$trts == transp.add$trt)
-
-# png("forest-transp.png", width = 8, height =2.5, res = 320, units = "in")
-forest(net.transp, small.values = "desirable", reference.group = "Control",
-       sortvar = "-Pscore",
-       add.data = transp.add,
-       leftcols = c("studlab", "Pscore", "resid"),
-       leftlabs = c(NA, NA, "rPOTH"),
-       #rightcols = c("Pscore", "resid", "effect", "ci"),
-       #rightlabs = c(NA, "rPOTH", NA, NA),
-       digits = 2, digits.prop = 3, digits.addcols = 3,
-       just.addcols = "right",
-       smlab = paste("Comparison: other vs control\nPOTH =",
-                     round(poth.transp$poth, 3)))
-
-# dev.off()
-
-# cumulative POTH
-cum.transp <- cumul(poth.transp)
-plot(cum.transp)
-# ggsave("cum_transp.png", width = 8, height = 4, units = "in")
-
-#subset POTH
-sub.transp <- subset(poth.transp,
-                     c("Fluconazole", "Itraconazole",
-                       "Liposomal amphotericin B", "Ketoconazole"))
-
-sub.transp # poth = 0.354
+sub3
